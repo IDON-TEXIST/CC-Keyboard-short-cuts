ee87c27fd5bb96078ae8841665837f2568caa1ef
Snowmanonahoe <waddwasad@gmail.com>
2020-11-18 20:08:18 -0500

    Changed url for new repo name

--- coolerindex.js
+++ coolerindex.js
@@ -10,7 +10,7 @@
 var code = "(" + (function() {
     var checkReady = setInterval(function() {
         if (typeof Game.ready !== 'undefined' && Game.ready) {
-            Game.LoadMod('https://idon-texist.github.io/cc-Keyboard-short-cuts/index.js');
+            Game.LoadMod('https://idon-texist.github.io/CC-Keyboard-short-cuts/index.js');
             clearInterval(checkReady);
         }
     }, 1000);
